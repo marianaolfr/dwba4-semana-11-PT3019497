4afd2c475bed3d19ee17f1c94f86c5c37aa19c6c
marianaolfr <91221958+marianaolfr@users.noreply.github.com>
2023-10-20 00:11:25 +0000

    Initial commit

---
+++ sobre.js
@@ -0,0 +1,37 @@
+ReactDOM.render(
+  'O Instituto Federal de Educação, Ciência e Tecnologia de São Paulo – é uma autarquia federal de ensino.',
+  document.getElementById("linha1")
+);
+
+ReactDOM.render(
+  'Fundada em 1909, como Escola de Aprendizes Artífices, é reconhecida pela sociedade paulista por sua excelência no ensino público gratuito de qualidade.',
+  document.getElementById("linha2")
+);
+
+ReactDOM.render(
+  'Durante sua história, recebeu, também, os nomes de Escola Técnica Federal de São Paulo e Centro Federal de Educação Tecnológica de São Paulo. Com a transformação em Instituto, em dezembro de 2008, passou a ter relevância de universidade, destacando-se pela autonomia.',
+  document.getElementById("linha3")
+);
+
+ReactDOM.render(
+  'Com a mudança, o Instituto Federal de São Paulo passou a destinar 50% das vagas para os cursos técnicos e, no mínimo, 20% das vagas para os cursos de licenciatura, sobretudo nas áreas de Ciências e da Matemática. Complementarmente, continuará oferecendo cursos de formação inicial e continuada, tecnologias, engenharias e pós-graduação.',
+  document.getElementById("linha4")
+);
+
+ReactDOM.render(
+  'Além dos cursos presenciais, o Instituto Federal de São Paulo oferece os cursos Técnicos em Administração e em Informática para Internet e, a partir de 2012, o superior de Formação de Professores na modalidade de Ensino a Distância (EaD).',
+  document.getElementById("linha5")
+);
+
+ReactDOM.render(
+  'O IFSP é organizado em diversos câmpus e possui mais de 40 mil alunos matriculados nas 36 unidades distribuídas pelo estado de São Paulo.;',
+  document.getElementById("linha6")
+);
+
+ReactDOM.render(
+  'Localizado em Rua Pedro Vicente, 625 - Canindé - São Paulo - SP - Brasil - Cep: 01109-010',
+  document.getElementById("linha7")
+);
+
+
+
